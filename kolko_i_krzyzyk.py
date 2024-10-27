90816ad36334e803f9cd2f5caa32f0f685697a5e
Monika <monia15114@gmail.com>
2024-10-27 17:01:28 +0100

    kolko i krzyzyk - end

--- kolko_i_krzyzyk.py
+++ kolko_i_krzyzyk.py
@@ -39,10 +39,19 @@ def wprowadzanie_o (tab):
     while True:
         w = random.randrange(0,3)
         k = random.randrange(0,3)
-        print (w,k)
         if tab[w][k] == " ":
             tab[w][k] = "O"
             break
+
+def wprowadzanie_o_uzytkownika (tab):
+    while True:
+        w = wiersz() - 1
+        k = kolumna() - 1
+        if tab[w][k] == " ":
+            tab[w][k] = "O"
+            break
+        else:
+                print("Te pole jest już zajęte. Wybierz inne.")
 
 
 def wyswietlanie(tab):
@@ -94,10 +103,29 @@ def sprawdzenie (tab):
         print("Wygrał O!")
         exit ()
 
-
-
 def puste_pola(tab):
     return any(" " in row for row in tab)
+
+def rodzaj_gry():
+    while True:
+        try:
+            t = int(input("Podaj 1 lub 2, aby wybrać rodzaj gry: "))
+            if t == 1:
+                return t
+            elif t == 2:
+                return t
+        except ValueError:
+               print("Podaj poprawnu numer odpowiadający trybowi gry")
+
+
+# wybor sposobu gry
+print ("Witaj w grze kółko i krzyżyk!")
+print ("W gre możesz zagrać w dwóch wariantach:")
+print ("1 - gra z komputerem (komputer wybiera pola losowo)")
+print ("2 - gra z innym użytkownikiem (na zmianę podajecie wybrane pola)")
+print (" ")
+
+t=rodzaj_gry()
 
 # tablica do gry 3x3
 tab = [[" ", " " ," "], [" ", " " ," "],[" ", " " ," "]]
@@ -106,15 +134,34 @@ while i < 3:
     print(tab[i])
     i = i + 1
 
-# gra
-while puste_pola(tab):
-    wprowadzanie_x(tab)
-    wyswietlanie(tab)
-    sprawdzenie(tab)
-    print ("Teraz komputer")
-    wprowadzanie_o(tab)
-    wyswietlanie(tab)
-    sprawdzenie(tab)
+
+if t == 1:
+    while puste_pola(tab):
+        wprowadzanie_x(tab)
+        wyswietlanie(tab)
+        sprawdzenie(tab)
+        print ("Teraz komputer")
+        wprowadzanie_o(tab)
+        wyswietlanie(tab)
+        sprawdzenie(tab)
+
+if t == 2:
+    while puste_pola(tab):
+        print("Teraz X")
+        wprowadzanie_x(tab)
+        wyswietlanie(tab)
+        sprawdzenie(tab)
+        print("Teraz O")
+        wprowadzanie_o_uzytkownika(tab)
+        wyswietlanie(tab)
+        sprawdzenie(tab)
+
+
+
+
+
+
+
 
 
 
